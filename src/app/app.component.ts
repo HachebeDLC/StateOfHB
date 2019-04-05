a614918573cc5478f671fe24f3103c58aac7f52a
HachebeDLC <hbgam3r@hotmail.es>
2019-04-05 22:25:57 +0200

    CSS y HTML terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'StateOfHB';
+  frasesHachebe =  [
+    'Ya está otra vez el cabrón de Hachebé ebrio',
+    'HBorracho',
+    'El cabrón de Hachebé está bebiendo ya como si fuese agua',
+    'Pero cómo puede ser que esté ya borracho el cabrón de Hachebé?',
+    'Hachebé está borracho. Sorprendente. Inaudito'
+  ];
+  open() {
+    const frase = this.frasesHachebe[(Math.floor(Math.random() * this.frasesHachebe.length))];
+    window.open('https://twitter.com/intent/tweet?text=' + frase);
+  }
 }
