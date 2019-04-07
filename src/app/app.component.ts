895579e08a9e4d2f99b1d51a343502ae9db5dc72
HachebeDLC <hbgam3r@hotmail.es>
2019-04-07 10:57:46 +0200

    Llamadas HTTP REST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+export interface Frase {
+  frase: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  frasesHachebe =  [
-    'Ya está otra vez el cabrón de Hachebé ebrio',
-    'HBorracho',
-    'El cabrón de Hachebé está bebiendo ya como si fuese agua',
-    'Pero cómo puede ser que esté ya borracho el cabrón de Hachebé?',
-    'Hachebé está borracho. Sorprendente. Inaudito'
-  ];
+  constructor(private http: HttpClient) {
+
+  }
   open() {
-    const frase = this.frasesHachebe[(Math.floor(Math.random() * this.frasesHachebe.length))];
-    window.open('https://twitter.com/intent/tweet?text=' + frase);
+    let fraseHB: Frase[] = [];
+    this.http.get<Frase[]>('/frases.json').subscribe(data => {
+      fraseHB = data;
+      const frase = fraseHB[(Math.floor(Math.random() * fraseHB.length))].frase;
+      window.open('https://twitter.com/intent/tweet?text=' + frase);
+    });
+
   }
 }
